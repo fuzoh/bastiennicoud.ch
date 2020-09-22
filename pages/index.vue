1fbcae31b7c56d9bc6a09b319049ee1ca440867c
Bastien <bastien.nicoud@gmail.com>
2020-09-22 16:21:39 +0200

    Nuxt base installation.

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        Bastien Nicoud
+      </h1>
+      <p>En construction</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Helvetica Neue',
+    Roboto,
+    Arial,
+    sans-serif;
+  display: block;
+  font-size: 68px;
+}
+</style>
