fd97742d97a733545e684ff4cc28fd983e17c2ee
Bastien <info@bastiennicoud.ch>
2021-03-23 20:33:07 +0100

    Shorten page transition animation

--- pages/index.vue
+++ pages/index.vue
@@ -4,19 +4,19 @@
       <my-abstract></my-abstract>
     </div>
     <nuxt-link class="main_grid_box main_right_up" :to="localePath('/skills')">
-      <h1>Hello</h1>
+      <h1>Skills</h1>
     </nuxt-link>
     <nuxt-link
       class="main_grid_box main_right_middle"
       :to="localePath('/technologies')"
     >
-      <h1>Hello</h1>
+      <h1>Technologies</h1>
     </nuxt-link>
     <nuxt-link
       class="main_grid_box main_right_down"
       :to="localePath('/contact')"
     >
-      <h1>Hello</h1>
+      <h1>Contact</h1>
     </nuxt-link>
   </main>
 </template>
